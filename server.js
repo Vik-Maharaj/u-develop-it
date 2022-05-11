dc6ac01922a09fbf31164d7c2d706a8b70633ab7
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-11 19:53:41 -0400

    Update server routes

--- server.js
+++ server.js
@@ -28,6 +28,23 @@ app.get('/api/candidates', (req, res) => {
     });
   });
 
+  // Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
