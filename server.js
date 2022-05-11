e691a7d18075ccab77d623efd477200ad9bcd54c
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-11 19:52:02 -0400

    Add server routes

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
