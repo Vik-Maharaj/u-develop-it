605d2571d2d8aaf9fdf460b18d770b2fcbcafb43
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-12 13:35:46 -0400

    Modularize connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 const express = require('express');
 const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
